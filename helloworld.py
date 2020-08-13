964d33fef99f45c7814dd5a00ddc1ed06d9c816a
Vishnu Soman K <69310229+vsk-coding@users.noreply.github.com>
2020-08-13 15:00:31 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,5 @@
 print("Hello World! I'm on Git now\n")
-print("cool now it's interesting....\n")
-print("ok i'm working on slave\n")
-print("windows 10 working ")
-print("now another test ")
-print("on pipeline demo")
-print("is this working")
-print("hey there!")
+
 
 
 def foo(bar=[]):        
